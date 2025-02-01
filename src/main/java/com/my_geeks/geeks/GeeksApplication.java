82ac19b7a33d80b3b77815c5974c3bc125369c60
bak3839 <bak3839@naver.com>
2025-02-01 15:06:33 +0900

    chore: 빌드 Dockerfile 추가

--- src/main/java/com/my_geeks/geeks/GeeksApplication.java
+++ src/main/java/com/my_geeks/geeks/GeeksApplication.java
@@ -1,8 +1,16 @@
 package com.my_geeks.geeks;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {
+		@Server(url = "http://localhost:8080", description = "로컬 URL"),
+		@Server(url = "https://server.my-geeks.com", description = "서버 URL")
+})
+@EnableJpaAuditing
 @SpringBootApplication
 public class GeeksApplication {
 
